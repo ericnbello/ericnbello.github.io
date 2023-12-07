1ab3224e254c99d1bf9e914547ff246d1823bcef
Eric Bello <ebello219@gmail.com>
2023-12-07 16:10:18 -0500

    removed github actions

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-    // output: 'standalone',
+    output: 'export',
     exportPathMap: async function (defaultPathMap) {
       return {
         '/': { page: '/' },
